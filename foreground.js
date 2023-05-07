4338191378658363c6930214c9abb6be05010e97
Peter Lindberg <peternlindberg@hotmail.com>
2023-05-06 21:15:44 -0700

    add a bunch of stuff

--- foreground.js
+++ foreground.js
@@ -5,3 +5,28 @@
 // and injected into the same or different pages.
 
 console.log("This prints to the console of the page (injected only if the page url matched)")
+console.log("this is something else")
+
+// Send a message to the background script of type "test"
+chrome.runtime.sendMessage({type: 'test', data: 'hello from the page'}, response => {
+    console.log("sending test message to background script")
+    console.log(response)
+})
+
+
+// function to insert a span with a smiley emoji immediately after this element
+function insert_smiley(element) {
+    const smiley = document.createElement("span")
+    smiley.innerHTML = "😊"
+    element.after(smiley)
+}
+
+// function to find all elements with the class "author" and insert a smiley after each one
+function insert_smileys() {
+    const elements = document.getElementsByClassName("author")
+    for (let element of elements) {
+        insert_smiley(element)
+    }
+}
+
+insert_smileys()
--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,24 @@ importScripts('service-worker-utils.js')
 // If you want to import a file that is deeper in the file hierarchy of your
 // extension, simply do `importScripts('path/to/file.js')`.
 // The path should be relative to the file `manifest.json`.
+
+const api_base_url = 'http://fakedetectapi-env.eba-q5xfkcgv.us-west-2.elasticbeanstalk.com/'
+
+// handle a "test" message from the foreground script by logging it
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.type === 'test') {
+            console.log(message)
+            // send response
+            sendResponse({data: 'hello from the service worker'})
+        }
+    }
+)
+
+// send a request to the fake detect api users/<username> endpoint, passing the username parameter, return the response
+async function get_user(username) {
+    console.log("making an api request to get user " + username)
+    const response = await fetch(api_base_url + 'users/' + username)
+    return response.json()
+}
+
+get_user("spez").then(response => console.log(response))
